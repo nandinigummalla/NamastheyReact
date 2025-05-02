88f259c697998e40abe565ab62f0f25bda58a92c
nandinigummalla <nandinigummalla27@gmail.com>
2025-05-02 18:12:16 +0530

    episode 5.2 useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,49 @@
+import { useState } from "react";
 import resArray from "../utils/mockData";
 import ResCard from "./ResCard";
 // passing props to component is passing arguments to function
 const Body = () => {
+  const [filteredRestaurants, setFilteredRestaurants] = useState(resArray);
+  const [searchTerm, setSearchTerm] = useState("");
+  const getTopRatedRestaurants = () => {
+    setFilteredRestaurants(
+      filteredRestaurants.filter((res) => res?.info.avgRating > 4)
+    );
+  };
+  const searchRest = () => {
+    setFilteredRestaurants(
+      filteredRestaurants.filter((rest) =>
+        rest?.info?.name?.toLowerCase()?.includes(searchTerm.toLowerCase())
+      )
+    );
+  };
+  const resetFilter = () => {
+    setFilteredRestaurants(resArray);
+  };
   return (
-    <div className="res-container">
-      {resArray?.map((rest) => (
-        <ResCard key={rest?.info?.id} resData={rest} />
-      ))}
+    <div className="body">
+      <div className="datafilter">
+        <div className="search">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search restaurants"
+          />
+          <button onClick={searchRest}>Search</button>
+        </div>
+        <div className="filter">
+          <button onClick={getTopRatedRestaurants}>
+            Top Rated Restaurants
+          </button>
+          <button onClick={resetFilter}>Reset</button>
+        </div>
+      </div>
+      <div className="res-container">
+        {filteredRestaurants?.map((rest) => (
+          <ResCard key={rest?.info?.id} resData={rest} />
+        ))}
+      </div>
     </div>
   );
 };
